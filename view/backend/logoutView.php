c1cb6b8625c339b43f228dbff59400f3f0031c7b
katell <katell.fracassi@gmail.com>
2018-09-28 13:33:31 +0200

    logout

--- view/backend/logoutView.php
+++ view/backend/logoutView.php
@@ -1,6 +1,10 @@
-<p>Vous êtes déconnectés</p>
 
+<div class="container text-center logout">
 
-<a class="nav-link" href="index.php?action=register">Se connecter <span class="sr-only">(current)</span></a>
-<a class="nav-link" href="index.php?action=home">retour au site</a>
+    <p class="font_size_3">Vous êtes déconnectés</p>
+
+    <a href="index.php?action=register" class="btn btn-info btn-lg active button_logout margin-right15">Se connecter <span class="sr-only">(current)</span></a>
+    <a href="index.php?action=home" class="btn btn-info btn-lg active button_logout margin-left15">retour au site</a>
 <!-- faire un bouton se connecter et un bouton dirigeant vers site sans connexion -->
+
+</div>
