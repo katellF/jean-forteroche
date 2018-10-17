25b7131bbdd3f64d0c69edfbaefb79e329bcbac5
katell <katell.fracassi@gmail.com>
2018-10-17 17:34:27 +0200

    update template

--- view/template.php
+++ view/template.php
@@ -3,10 +3,10 @@
 
 <head>
 
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php
-        require (dirname(__FILE__) . '/includes/scripts_head.php');
+    require(dirname(__FILE__) . '/includes/scripts_head.php');
     ?>
 
     <title><?= $title ?></title>
@@ -17,71 +17,39 @@
 
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-<!--        <a class="navbar-brand" href="#">Navbar</a>-->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+                    aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-<!--        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">-->
-        <div class=" collapse navbar-collapse row justify-content-end menu_burger" id="navbarTogglerDemo03">
-            <ul class="navbar-nav mt-2 mt-lg-0">
-                <li class="nav-item active border_nav color_link">
-<!--                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>-->
-                    <a class="nav-link color_link" href="index.php?action=home">Accueil <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active border_nav color_link">
-<!--                    <a class="nav-link" href="#">Link</a>-->
-                    <a class="nav-link color_link" href="index.php?action=writer">A Propos</a>
+            <div class=" collapse navbar-collapse row justify-content-end menu_burger" id="navbarTogglerDemo03">
+                <ul class="navbar-nav mt-2 mt-lg-0">
+                    <li class="nav-item active border_nav color_link">
+                        <a class="nav-link color_link" href="index.php?action=home">Accueil <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active border_nav color_link">
 
-                <li class="nav-item active border_nav color_link">
-<!--                    <a class="nav-link disabled" href="#">Disabled</a>-->
-                    <a class="nav-link color_link" href="index.php?action=listPosts">Chapitres</a>
-                </li>
-                <li class="nav-item active border_nav">
-<!--                    <a class="nav-link disabled" href="#">Disabled</a>-->
-                    <a class="nav-link color_link" href="index.php?action=contact">Contact</a>
-                </li>
-            </ul>
-<!--            <form class="form-inline my-2 my-lg-0">-->
-<!--                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">-->
-<!--                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>-->
-<!--            </form>-->
+                        <a class="nav-link color_link" href="index.php?action=writer">A Propos</a>
+
+                    <li class="nav-item active border_nav color_link">
+
+                        <a class="nav-link color_link" href="index.php?action=listPosts">Chapitres</a>
+                    </li>
+                    <li class="nav-item active border_nav">
+
+                        <a class="nav-link color_link" href="index.php?action=contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
 
         </div>
-
-        </div>
-
-
     </nav>
-
-<!--    <nav class="navbar fixed-top  navbar-expand-lg navbar-dark bg-dark">-->
-<!--   <div class="container">-->
-<!--<!--      <img src="" alt="">Aside logo book-->
-<!--        <div class="collapse navbar-collapse row justify-content-end" id="navbarNavDropdown">-->
-<!--            <ul class="navbar-nav">-->
-<!--                <li class="nav-item active">-->
-<!--                    <a class="nav-link" href="index.php?action=home">Accueil <span class="sr-only">(current)</span></a>-->
-<!--                </li>-->
-<!--                <li class="nav-item active ">-->
-<!--                    <a class="nav-link" href="index.php?action=writer">A Propos</a>-->
-<!--                </li>-->
-<!--                <li class="nav-item active">-->
-<!--                    <a class="nav-link" href="index.php?action=listPosts">Chapitres</a>-->
-<!--                </li>-->
-<!--                <li class="nav-item active">-->
-<!--                    <a class="nav-link" href="index.php?action=contact">Contact</a>-->
-<!--                </li>-->
-<!---->
-<!--            </ul>-->
-<!--        </div>-->
-<!--   </div>-->
-<!--    </nav>-->
 </header>
 <?= $content ?>
 
 
 <?php
-require (dirname(__FILE__) . '/includes/scripts_footer.php');
+require(dirname(__FILE__) . '/includes/scripts_footer.php');
 ?>
 
 </body>
@@ -90,19 +58,20 @@ require (dirname(__FILE__) . '/includes/scripts_footer.php');
 
     <nav class="navbar  navbar-expand-lg navbar-dark bg-dark">
 
-            <div class="collapse navbar-collapse row justify-content-center" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php?action=register">Admin <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Mentions Légales</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
+        <div class="collapse navbar-collapse row justify-content-center" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php?action=register">Admin <span
+                                class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Mentions Légales</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
 
     </nav>
 
