a63d83302cfa1631cac389cf7482b28bb0311321
katell <katell.fracassi@gmail.com>
2018-10-17 15:10:45 +0200

    responsif/menu burger

--- view/template.php
+++ view/template.php
@@ -15,28 +15,66 @@
 <body>
 <header class="container">
 
-    <nav class="navbar fixed-top  navbar-expand-lg navbar-dark bg-dark">
-   <div class="container">
-<!--        <img src="" alt="">Aside logo book-->
-        <div class="collapse navbar-collapse row justify-content-end" id="navbarNavDropdown">
-            <ul class="navbar-nav">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+<!--        <a class="navbar-brand" href="#">Navbar</a>-->
+
+<!--        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">-->
+        <div class="collapse navbar-collapse row justify-content-end" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mt-2 mt-lg-0">
                 <li class="nav-item active">
+<!--                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>-->
                     <a class="nav-link" href="index.php?action=home">Accueil <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item active ">
-                    <a class="nav-link" href="index.php?action=writer">A Propos</a>
-                </li>
                 <li class="nav-item active">
+<!--                    <a class="nav-link" href="#">Link</a>-->
+                    <a class="nav-link" href="index.php?action=writer">A Propos</a>
+                <li class="nav-item active">
+<!--                    <a class="nav-link disabled" href="#">Disabled</a>-->
                     <a class="nav-link" href="index.php?action=listPosts">Chapitres</a>
                 </li>
                 <li class="nav-item active">
+<!--                    <a class="nav-link disabled" href="#">Disabled</a>-->
                     <a class="nav-link" href="index.php?action=contact">Contact</a>
                 </li>
-
             </ul>
+<!--            <form class="form-inline my-2 my-lg-0">-->
+<!--                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">-->
+<!--                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>-->
+<!--            </form>-->
+
         </div>
-   </div>
+
+        </div>
+
+
     </nav>
+
+<!--    <nav class="navbar fixed-top  navbar-expand-lg navbar-dark bg-dark">-->
+<!--   <div class="container">-->
+<!--<!--      <img src="" alt="">Aside logo book-->
+<!--        <div class="collapse navbar-collapse row justify-content-end" id="navbarNavDropdown">-->
+<!--            <ul class="navbar-nav">-->
+<!--                <li class="nav-item active">-->
+<!--                    <a class="nav-link" href="index.php?action=home">Accueil <span class="sr-only">(current)</span></a>-->
+<!--                </li>-->
+<!--                <li class="nav-item active ">-->
+<!--                    <a class="nav-link" href="index.php?action=writer">A Propos</a>-->
+<!--                </li>-->
+<!--                <li class="nav-item active">-->
+<!--                    <a class="nav-link" href="index.php?action=listPosts">Chapitres</a>-->
+<!--                </li>-->
+<!--                <li class="nav-item active">-->
+<!--                    <a class="nav-link" href="index.php?action=contact">Contact</a>-->
+<!--                </li>-->
+<!---->
+<!--            </ul>-->
+<!--        </div>-->
+<!--   </div>-->
+<!--    </nav>-->
 </header>
 <?= $content ?>
 
