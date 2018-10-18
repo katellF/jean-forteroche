a1fa252020360d4f737f6717b5171a0cf91acf59
katell <katell.fracassi@gmail.com>
2018-10-18 08:35:56 +0200

    Update view/template_connect.php

--- view/template_connect.php
+++ view/template_connect.php
@@ -18,22 +18,29 @@
 <body>
 <header class="container">
 
-    <nav class="navbar fixed-top  navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <!--        <img src="" alt="">Aside logo book-->
-            <div class="collapse navbar-collapse row justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php?action=home">Accueil <span class="sr-only">(current)</span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+                    aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class=" collapse navbar-collapse row justify-content-end menu_burger" id="navbarTogglerDemo03">
+                <ul class="navbar-nav mt-2 mt-lg-0">
+                    <li class="nav-item active border_nav color_link">
+                        <a class="nav-link color_link" href="index.php?action=home">Accueil <span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item active ">
-                        <a class="nav-link" href="index.php?action=writer">A Propos</a>
+                    <li class="nav-item active border_nav color_link">
+
+                        <a class="nav-link color_link" href="index.php?action=writer">A Propos</a>
+
+                    <li class="nav-item active border_nav color_link">
+
+                        <a class="nav-link color_link" href="index.php?action=listPosts">Chapitres</a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php?action=listPosts">Chapitres</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php?action=contact">Contact</a>
+                    <li class="nav-item active border_nav">
+
+                        <a class="nav-link color_link" href="index.php?action=contact">Contact</a>
                     </li>
                     <li class="nav-item active">
                         <a class="nav-link" href="index.php?action=logout">Se deconnecter</a>
@@ -43,6 +50,7 @@
                     </li>
                 </ul>
             </div>
+
         </div>
     </nav>
 </header>
