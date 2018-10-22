87e30da461056f7aa027abebbcc4c7746944e482
katell <katell.fracassi@gmail.com>
2018-10-22 22:20:12 +0200

    Remove contactForm2

--- controller/frontend/ControllerContact.php
+++ controller/frontend/ControllerContact.php
@@ -11,53 +11,7 @@ class ControllerContact
         $this->ctrlConnect = new ControllerConnect();
     }
 
-    public function contactForm2()
-    {
-        session_start();
-        if ($this->ctrlConnect->isUserConnected()) {
-            if (isset ($_POST) && !empty($_POST)) {
-                $errorCounter = 0;
-
-                if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
-
-                    throw new Exception('Vous n avez pas ecrit l email correctement');
-                    $errorCounter++;
-                }
-
-                if ($errorCounter === 0) {
-                    $contact = $this->contactManager->insertContact();
-                    header('Location: index.php?action=contactsent');
-
-
-                }
-            } else{
-
-                $view = new View("frontend/contact");
-                $view->generate(array(), "template_connect");
-            }
-        } elseif (isset ($_POST) && !empty($_POST)) {
-            $errorCounter = 0;
-
-            if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
-
-                throw new Exception('Vous n avez pas ecrit l email correctement');
-                $errorCounter++;
-            }
-
-            if ($errorCounter === 0) {
-
-                $contact = $this->contactManager->insertContact();
-                header('Location: index.php?action=contactsent');
-            }
-
-
-        }else{
-
-            $view = new View("frontend/contact");
-            $view->generate(array());
-        }
-    }
-
+   
     public function contactForm()
     {
         session_start();
